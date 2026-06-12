5f0e9e6b92ffd1d6e7498a6c866ac9a08675d678
renseimacron <genfuruichi@gmail.com>
2026-06-12 10:30:09 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'health-supervisor-v3';
+const CACHE_NAME = 'health-supervisor-v5';
 const ASSETS = [
   './',
   './index.html',
@@ -15,7 +15,7 @@ self.addEventListener('install', event => {
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(keys.filter(k => k.startsWith('health-supervisor-') && k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())
   );
 });
 
